16fd593a39a711e66eda5711813a174ff2941812
TusharChakraborty07 <chakrabortytushar684@gmail.com>
2026-04-18 23:37:53 +0530

    added post model and new UI

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const bcrypt = require("bcrypt");
 const path = require("path");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 // Model's
 const userModel = require("./model/userModel");
-const cookieParser = require("cookie-parser");
+const postModel = require("./model/postModel");
 
 const app = express();
 
@@ -15,8 +17,36 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+app.get("/register", (req, res) => {
   res.render("index");
+});
+
+// User Create
+app.post("/register", async (req, res) => {
+  let { username, name, age, email, password } = req.body;
+  const exitsUser = await userModel.findOne({ email });
+
+  if (exitsUser) return res.status(500).send("User already exits");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      const user = await userModel.create({
+        username,
+        name,
+        age,
+        email,
+        password: hash,
+      });
+
+      const token = jwt.sign({ email: email, userid: user._id }, "SECRET_KEY");
+      res.cookie("token", token);
+      res.send("User Created Sucessfully");
+    });
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 app.listen(3000, () => {
